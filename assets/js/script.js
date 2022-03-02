af68f7dc1c1d16e8088d386a1ae22a448f4aaf2b
Estelindis <estelindis@gmail.com>
2022-03-02 10:26:46 +0000

    Add basic script structure

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const storyNode = document.getElementById('story');
+const choiceArray = document.getElementById('choice-box');
+
+let state = {};
+
+function newStory() {
+
+}
+
+function showStoryNode(storyNodeList) {
+
+}
+
+function makeChoice(choice) {
+
+}
+
+newStory();
