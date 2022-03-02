5983e95226e8be9452cbd8ef9fd67eb683d8d44e
Estelindis <estelindis@gmail.com>
2022-03-02 22:21:35 +0000

    Fix some typos, edit some story text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 let storyElement = document.getElementById('story');
 let choiceElements = document.getElementById('choice-box');
 
@@ -30,7 +32,7 @@ const storyNodes = [
     {
         id: 1,
         ref: 'story03node01',
-        story: 'Some people go through life swimming upstream.  Fighting the force of the current.  Not me.  I let it carry me along.  I can\'t say the river\'s always taken me where I wanted, but it never tired me out.  If I haven\'t been happy, at least I\'ve been content.  Lately, though, something\'s different.  I see glimpses of strange things in the corner of my eye.  Whenever look at them, they vanish.  Something just isn\'t right.',
+        story: 'Some people go through life swimming upstream.  Fighting the force of the current.  Not me.  I let it carry me along.  I can\'t say the river\'s always taken me where I wanted, but it never hurt me.  If I haven\'t been happy, at least I\'ve been content.  Lately, though, something\'s different.  I see glimpses of strange things in the corner of my eye.  Whenever look at them, they vanish.  Something just isn\'t right.',
         choices: [
             {
                 choice: 'Why look at all?  I ignore the strangeness.',
@@ -62,10 +64,10 @@ const storyNodes = [
     {
         id: 3,
         ref: 'story03node02b',
-        story: 'When I try to look at the anomaly quickly, it vanishes.  So I try slowly.  Unfocusing my eyes a little, I let my vision drift, moving casually towards the shifting light and colours.  It looks like... a curtain.  Flickering at the edge of my sight.  Billowing in the wind, even though the air around me is utterly still.  When I try to make out more detail, it vanishes.  So my days pass, trying to see more of the strangeness, but not so much that it flees from my fight.  One day I see a vase.  Then a fork.  Then, for the barest moment, a diamond ring on a shadowed hand.  The jewel flashes like the sun and is gone.  As my vision adjusts, I begin to hear a song.  So familiar, yet I can\'t place where I last heard it.  I turn to find the singer, but no-one\'s there.  I\'m completely alone.',
+        story: 'When I look at the anomaly quickly, it vanishes.  So I look slowly.  Unfocusing my eyes, I let my vision drift, moving casually towards the anomaly.  It looks like... a curtain.  Flickering at the edge of my sight.  Billowing in the wind, though the air around me is utterly still.  When I try to make out more detail, it fades away.  \r\n And so my days pass, trying to see more of the strangeness, but not so much that it flees from my sight.  One day I see a vase.  Then a fork.  Then, for the barest moment, a diamond ring on a shadowed hand.  The jewel flashes like the sun and is gone.  As my vision adjusts, I begin to hear a song.  So familiar, yet I can\'t place where I last heard it.  I turn to find the singer, but no-one\'s there.  I\'m completely alone.',
         choices: [
             {
-                choice: 'I must be losing my mind.  I try to start ignoring the strangeness.',
+                choice: 'I try to ignore all anomalies.',
                 nextStoryNode: 'story03node03c'
             },
             {
