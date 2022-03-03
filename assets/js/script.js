467411126ad01cb9aed62f9d058f61550fae1621
Estelindis <estelindis@gmail.com>
2022-03-03 11:59:11 +0000

    Add storyNodes 4-10, link nodes 2-10

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,7 +38,7 @@ const storyNodes = [
                 nextStoryNode: 2
             },
             {
-                choice: 'Carefully, I examine the odd phenomena.',
+                choice: 'Carefully, I examine the strangeness.',
                 nextStoryNode: 3
             }
         ]
@@ -48,10 +48,16 @@ const storyNodes = [
         story: 'Life continues as always.  Never anything worth describing, for better or worse.  No triumph.  No suffering.  Only the knowledge that tomorrow will be the same as yesterday.  Sometimes anomalies flash at the edge of my vision, but I pay them no heed.  Then, one day, I hear a snatch of a song.  So familiar, yet I can\'t place where I heard it.  I turn to find the singer, but no-one\'s here.  I\'m completely alone.',
         choices: [
             {
-                choice: 'I ignore the song as well.'
+                choice: 'Am I losing my mind?',
+                nextStoryNode: 5
             },
             {
-                choice: 'I close my eyes and listen.'
+                choice: 'I ignore the song as well.',
+                nextStoryNode: 4
+            },
+            {
+                choice: 'I close my eyes and listen.',
+                nextStoryNode: 10
             }
         ]
     },
@@ -60,10 +66,122 @@ const storyNodes = [
         story: 'When I look at the anomaly quickly, it vanishes.  So I look slowly.  Unfocusing my eyes, I let my vision drift, moving casually towards the anomaly.  It looks like... a curtain.  Flickering at the edge of my sight.  Billowing in the wind, though the air around me is utterly still.  When I try to make out more detail, it fades away.  \r\n And so my days pass, trying to see more of the strangeness, but not so much that it flees from my sight.  One day I see a vase.  Then a fork.  Then, for the barest moment, a diamond ring on a shadowed hand.  The jewel flashes like the sun and is gone.  As my vision adjusts, I begin to hear a song.  So familiar, yet I can\'t place where I heard it.  I turn to find the singer, but no-one\'s here.  I\'m completely alone.',
         choices: [
             {
-                choice: 'I try to ignore all anomalies.'
+                choice: 'Am I losing my mind?',
+                nextStoryNode: 5
             },
             {
-                choice: 'I close my eyes and listen.'
+                choice: 'I try to ignore all anomalies.',
+                nextStoryNode: 4
+            },
+            {
+                choice: 'I close my eyes and listen.',
+                nextStoryNode: 10
+            }
+        ]
+    },
+    {
+        id: 4,
+        story: 'Life continues as always - mostly.  Little worth describing, except for that strange song.  The visual anomalies I can ignore.  But the song persists.  Already familiar when I first heard it, it becomes more so.  Frustratingly, I can\'t quite understand the words.  They sound like half-remembered syllables that should make perfect sense but don\'t.',
+        choices: [
+            {
+                choice: 'Am I losing my mind?',
+                nextStoryNode: 5
+            },
+            {
+                choice: 'I try to block out the song.',
+                nextStoryNode: 9
+            },
+            {
+                choice: 'I close my eyes and listen.',
+                nextStoryNode: 10
+            }
+        ]
+    },
+    {
+        id: 5,
+        story: 'The question is worth asking.  But how to answer it?  If I\'m broken, can I self-diagnose?  Can I make sense of sounds and sights that make no sense at all?',
+        choices: [
+            {
+                choice: 'I try to work it out logically.',
+                nextStoryNode: 6
+            },
+            {
+                choice: 'Forget logic.  How do I feel?',
+                nextStoryNode: 7
+            },
+            {
+                choice: 'Forget I asked; ignore the question.',
+                nextStoryNode: 8
+            }
+        ]
+    },
+    {
+        id: 6,
+        story: 'Either there\'s something wrong with me, or something wrong with the world.  I can\'t know which.  But I need to pay attention to the strange signals I\'m getting.  I won\'t find answers by ignoring the evidence.  Still, part of me fears what I might find out.',
+        choices: [
+            {
+                choice: 'I close my eyes and listen.',
+                nextStoryNode: 10
+            },
+            {
+                choice: 'I try to block out the song.',
+                nextStoryNode: 9
+            }
+        ]
+    },
+    {
+        id: 7,
+        story: 'I feel... alone.  I can\'t remember my last meaningful interaction.  When have I confessed my deepest fears to someone or received a hug?  I can\'t remember, yet I can\'t accept that no such thing ever happened.  Whatever else the song is, at least it\'s the voice of another person.  I need that voice.  But... can I accept what it has to say?',
+        choices: [
+            {
+                choice: 'I close my eyes and listen.',
+                nextStoryNode: 10
+            },
+            {
+                choice: 'I try to block out the song.',
+                nextStoryNode: 9
+            }
+        ]
+    },
+    {
+        id: 8,
+        story: 'So what now?  Do I try to make out more about the maddening, evasive song?  Or do I try to block it out forever?',
+        choices: [
+            {
+                choice: 'I close my eyes and listen.',
+                nextStoryNode: 10
+            },
+            {
+                choice: 'I try to block out the song.',
+                nextStoryNode: 9
+            }
+        ]
+    },
+    {
+        id: 9,
+        story: 'I put my fingers in my ears, but it\'s no use.  The song isn\'t coming from beyond.  It\'s within me.',
+        choices: [
+            {
+                choice: 'I close my eyes and listen.',
+                nextStoryNode: 10
+            },
+            {
+                choice: 'I try to ignore it nonetheless.',
+                nextStoryNode: 12
+            }
+        ]
+    },
+    {
+        id: 10,
+        story: 'The song is simple, almost child-like.  Verse and chorus go back and forth with so little variation that one might call it boring.  Yet there\'s something comforting about it.  Like a warm drink on a cold night.',
+        choices: [
+            {
+                choice: 'I listen more.',
+                nextStoryNode: 16
+            },
+            {
+                choice: 'I try to block out the song.',
+                nextStoryNode: 11
             }
         ]
     }
